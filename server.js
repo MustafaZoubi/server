8a4d1de9ec2443eaa14f535e1cca7453808f2a24
MustafaZoubi <220932775+MustafaZoubi@users.noreply.github.com>
2026-01-21 12:52:01 +0300

    Starting the server

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 import express from "express"
 import connectDB from "./config/db";
+import app from "./app";
+
 
 dotenv.config();
 
 await connectDB();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
 
 
 

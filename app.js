0797717ee5df8c0e6e28f0d4cb447a0140fc56b2
MustafaZoubi <220932775+MustafaZoubi@users.noreply.github.com>
2026-01-22 03:42:12 +0300

    Finsih setting up the cart backend

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import gameRoutes from "./routes/gameRoutes.js"
 import achievementRoutes from "./routes/achievementRoutes.js"
+import cartRoutes from "./routes/cartRoutes.js";
+
 const app = express();
 
 app.use(express.json());
@@ -16,5 +18,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/games", gameRoutes);
 app.use("/api/achievements", achievementRoutes);
+app.use("/api/cart", cartRoutes);
 
 export default app;

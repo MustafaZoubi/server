fa66a7c4be97edc9da3227c6b43231a5692dba35
MustafaZoubi <220932775+MustafaZoubi@users.noreply.github.com>
2026-01-22 21:40:57 +0300

    fdsfds

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         "http://localhost:5174",
-        "https://client-production-3146.up.railway.app/"
+        "https://client-production-3146.up.railway.app"
     ],
     credentials: true
 }));

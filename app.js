830482c67aed65912cede9486e2a6eda0c688221
MustafaZoubi <220932775+MustafaZoubi@users.noreply.github.com>
2026-01-31 13:04:15 +0300

    Fix Omar

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         "http://localhost:5173",
+        "https://client-production-3146.up.railway.app"
     ],
     credentials: true
 }));
